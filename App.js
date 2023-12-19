9a2582364ce7c1daf8eb5ce4182c61f8df5f3eb2
M Bilal <bilaldarmuhammad@gmail.com>
2023-12-19 21:50:30 +0500

    add components

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Header from './src/sections/Header';
+import Card from './src/sections/Card';
+import Transactions from './src/sections/Transactions';
+import { useColorScheme } from 'nativewind';
+import { useFonts } from 'expo-font';
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+  const [fontsLoaded, fontError] = useFonts({
+    SpaceGroteskSemiBold: require("./src/font/SpaceGrotesk-SemiBold.ttf"),
+    SpaceGroteskBold: require("./src/font/SpaceGrotesk-Bold.ttf"),
+    SpaceGroteskMedium: require("./src/font/SpaceGrotesk-Medium.ttf"),
+  });
   return (
-    <Text>Hello world</Text>
+    <SafeAreaView className="p-6 dark:bg-neutral-900">
+      <StatusBar style='auto' />
+      <View>
+        <View className="my-6">
+          <Header />
+
+          <Card />
+
+
+          <Transactions />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
